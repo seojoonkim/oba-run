3162adab58e85b486cddd6b99860c72540671fe0
Seojoon Kim <seojoon.kim@gmail.com>
2026-03-21 08:46:59 +0900

    Redirect foa.wtf to oba.wtf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "foa.wtf",
+          },
+        ],
+        destination: "https://oba.wtf/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
